85eb2f2aafabc5e81714ad2a366acea2a6d7288f
Son Truong <sontruong@pixelcent.com>
2023-12-14 18:19:52 +0700

    Galaxy Background <3

---
+++ GalaxyBackground/index.tsx
@@ -0,0 +1,22 @@
+// import { memo } from "react";
+// import classes from "./styles.module.scss";
+// import clsx from "clsx";
+
+// interface GalaxyBackgroundProps {
+//   className?: string;
+// }
+
+// const GalaxyBackground: React.FC<GalaxyBackgroundProps> = memo((props: GalaxyBackgroundProps) => {
+//   const { className } = props;
+
+//   return (
+//     <div className={clsx(classes.container, className)}>
+//       <div className={classes.stars1} />
+//       <div className={classes.stars2} />
+//       <div className={classes.stars3} />
+//       <div className={classes.content}>Hello World!</div>
+//     </div>
+//   );
+// });
+
+// export default GalaxyBackground;
